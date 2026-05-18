5b0f5d0d495a250f6da8218f80c12a9f91918231
Abayomi Akintomide <ywordk@gmail.com>
2026-05-18 22:56:42 +0100

    Solve Max Consecutive Ones

---
+++ 0485-max-consecutive-ones/max-consecutive-ones.ts
@@ -0,0 +1,22 @@
+function findMaxConsecutiveOnes(nums: number[]): number {
+    let max = 0;
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 1) {
+            count++;
+        } else {
+            if (count > max) {
+                max = count;
+            }
+
+            count = 0;
+        }
+    };
+
+    if (count > max) {
+        return count;
+    }
+
+    return max;
+};
